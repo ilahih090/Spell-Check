8acdbdf847203e4534be57688e0e7bf16075ffb0
ilahih090 <illahih090@gmail.com>
2025-02-12 17:33:03 +0500

    Virtual Environment + Main code change

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 from flask import Flask, render_template, request
-from spellchecker import SpellChecker  # Import SpellChecker
+from spellchecker import SpellChecker  
 
 app = Flask(__name__, static_folder='static')
 
-spell = SpellChecker()  # Initialize spell checker
+spell = SpellChecker()  
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     corrected_text = None
     input_text = ""
+    suggestions = {}
 
     if request.method == 'POST':
         input_text = request.form['input_text']
-        words = input_text.split()  # Split input into words
-        corrected_words = [spell.correction(word) if spell.correction(word) else word for word in words]  
-        corrected_text = " ".join(corrected_words)  # Rejoin words
+        words = input_text.split()  
 
-    return render_template('index.html', corrected_text=corrected_text, input_text=input_text)
+        corrected_words = []
+        for word in words:
+            correction = spell.correction(word)
+            corrected_words.append(correction if correction else word)
+
+            # Get spelling suggestions for misspelled words
+            if word not in spell:
+                suggestions[word] = spell.candidates(word)
+
+        corrected_text = " ".join(corrected_words)  
+
+    return render_template('index.html', corrected_text=corrected_text, input_text=input_text, suggestions=suggestions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
